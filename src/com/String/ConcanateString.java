c4d1127a18d9733f207a95492b8d984718937d5d
karanwaghachoure95 <karanwaghachoure95@gmail.com>
2025-07-02 14:51:52 +0530

    Program

---
+++ src/com/String/ConcanateString.java
@@ -0,0 +1,11 @@
+package com.String;
+
+public class ConcanateString {
+	public static void main(String[] args) {
+		String str1="the ";
+		String str2="Kiran";
+		
+		System.out.println(str1+str2);
+	}
+
+}

a550c40036d521f818104600d1fb579fc2053787
karanwaghachoure95 <karanwaghachoure95@gmail.com>
2025-07-02 16:03:08 +0530

    Print the Number

--- src/com/String/ForLoop.java
+++ src/com/String/ForLoop.java
@@ -2,6 +2,9 @@ package com.String;
 
 public class ForLoop {
 	public static void main(String[] args) {
+		System.out.println("-----------------------");
+		System.out.println("Print the number 0 to 10");
+		System.out.println("-------------------------");
 		for(int i=0;i<=10;i++) {
 			System.out.println(i);
 		}
